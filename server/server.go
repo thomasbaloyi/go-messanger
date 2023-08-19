6e7a655b60b64f6fed1d8e1f2d69796e1b16c638
Thomas Baloyi <thomas.baloyi@pathcare.net>
2023-08-19 15:08:00 +0200

    Expose random port to accept connections

--- server/server.go
+++ server/server.go
@@ -1,7 +1,35 @@
 package server
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
 
-func ListenAndServe() {
-	fmt.Println("Listening and serving...")
+func ListenAndServe(port string) {
+
+	listener, err := net.Listen("tcp4", "localhost:"+port)
+
+	if err != nil {
+		fmt.Println("Could not start server: ", err)
+		return
+	} else {
+		fmt.Println("Listening and serving on the address:", listener.Addr().String())
+	}
+
+	connection, err := listener.Accept()
+
+	if err != nil {
+		fmt.Println("Could not accept/open connection: ", err)
+	} else {
+		fmt.Println("Connection established: ", connection)
+	}
+
+	err = connection.Close()
+
+	if err != nil {
+		fmt.Println("Could not close connection: ", err)
+	} else {
+		fmt.Println("Connection closed: ", connection)
+	}
+
 }
